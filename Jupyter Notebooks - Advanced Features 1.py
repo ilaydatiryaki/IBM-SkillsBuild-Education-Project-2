3a599cb0c96b61ba4fa8f94785feb9cf2000ac85
İlayda TİRYAKİ <63457363+ilaydatiryaki@users.noreply.github.com>
2021-03-23 13:59:13 +0300

    Add files via upload

---
+++ Jupyter Notebooks - Advanced Features 1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+sys.version
+
+
+# # This is a Lab Notebook
+# This is fun!
+
+# In[2]:
+
+
+for i in range(1000):
+   print(i)
+
+
+# In[ ]:
+
+
+
+
